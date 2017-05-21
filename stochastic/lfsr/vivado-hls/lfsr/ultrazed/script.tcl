112cb3122e6060f8ade23204b20d556105819327
Timothy McPhillips <tmcphillips@absoluteflow.org>
2017-05-20 18:41:05 -0700

    Created Vivado HLS project and first solution targeting ultrazed board.

---
+++ stochastic/lfsr/vivado-hls/lfsr/ultrazed/script.tcl
@@ -0,0 +1,15 @@
+############################################################
+## This file is generated automatically by Vivado HLS.
+## Please DO NOT edit it.
+## Copyright (C) 1986-2017 Xilinx, Inc. All Rights Reserved.
+############################################################
+open_project lfsr
+add_files ../src/cpp/simple_lfsr.cpp
+open_solution "ultrazed"
+set_part {xczu3eg-sfva625-1-i-es1} -tool vivado
+create_clock -period 10 -name default
+#source "./lfsr/ultrazed/directives.tcl"
+#csim_design
+csynth_design
+#cosim_design
+export_design -format ip_catalog
---
+++ stochastic/lfsr/vivado-hls/lfsr/vivado_hls.app
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<project xmlns="com.autoesl.autopilot.project" top="">
+  <files>
+    <file name="../src/cpp/simple_lfsr.cpp" sc="0" tb="false" cflags=""/>
+  </files>
+  <solutions>
+    <solution name="ultrazed" status="active"/>
+  </solutions>
+  <includePaths/>
+  <libraryPaths/>
+  <Simulation>
+    <SimFlow name="csim"/>
+  </Simulation>
+</project>
