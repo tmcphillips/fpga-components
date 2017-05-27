2129cba4680f4a8e4fe1728d69555a07faeb6191
Timothy McPhillips <tmcphillips@absoluteflow.org>
2017-05-26 22:58:53 -0700

    Changed hex literals to binary literals in tests of LowerBitsMask.

--- stochastic/lfsr/visual studio/simple_lfsr_tests/lower_bits_mask_tests.cpp
+++ stochastic/lfsr/visual studio/simple_lfsr_tests/lower_bits_mask_tests.cpp
@@ -12,43 +12,44 @@ namespace simple_lfsr_tests
 		TEST_METHOD(TestLowerBitsMask_0_Bits)
 		{
 			simple_lfsr::LowerBitsMask<0> mask;
-			Assert::AreEqual(0x00u, mask.value);
+			Assert::AreEqual(0b0000u, mask.value);
 		}
 
 		TEST_METHOD(TestLowerBitsMask_1_Bit)
 		{
 			simple_lfsr::LowerBitsMask<1> mask;
-			Assert::AreEqual( 0x01u, mask.value );
+			Assert::AreEqual( 0b0001u, mask.value );
 		}
 
 		TEST_METHOD(TestLowerBitsMask_8_Bits)
 		{
 			simple_lfsr::LowerBitsMask<8> mask;
-			Assert::AreEqual( 0xffu, mask.value );
+			Assert::AreEqual( 0b1111'1111u, mask.value );
 		}
 
 		TEST_METHOD(TestLowerBitsMask_15_Bits)
 		{
 			simple_lfsr::LowerBitsMask<15> mask;
-			Assert::AreEqual( 0x7fffu, mask.value );
+			Assert::AreEqual( 0b0111'1111'1111'1111u, mask.value );
 		}
 
 		TEST_METHOD(TestLowerBitsMask_16_Bits)
 		{
 			simple_lfsr::LowerBitsMask<16> mask;
-			Assert::AreEqual(0xffffu, mask.value);
+			Assert::AreEqual(0b1111'1111'1111'1111u, mask.value);
 		}
 
 		TEST_METHOD(TestLowerBitsMask_31_Bits)
 		{
 			simple_lfsr::LowerBitsMask<31> mask;
-			Assert::AreEqual(0x7fffffffu, mask.value);
+			Assert::AreEqual(0b0111'1111'1111'1111'1111'1111'1111'1111u, mask.value);
 		}
 
 		TEST_METHOD(TestLowerBitsMask_32_Bits)
 		{
 			simple_lfsr::LowerBitsMask<32> mask;
-			Assert::AreEqual(0xffffffffu, mask.value);
+			Assert::AreEqual(0b1111'1111'1111'1111'1111'1111'1111'1111u, mask.value);
 		}
+
 	};
 }
